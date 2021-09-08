8f77ce28140ac89de89c0ab4d20aa28eea76051a
brksygmr <brksygmr@outlook.com>
2021-09-08 15:33:17 +0100

    printcomb

---
+++ printcomb.go
@@ -0,0 +1,22 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintComb() {
+	for a := '0'; a <= '7'; a++ {
+		for b := a + 1; b <= '8'; b++ {
+			for c := b + 1; c <= '9'; c++ {
+				for d := ','; d <= ','; d++ {
+					for e := ' '; e <= ' '; e++ {
+
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+						z01.PrintRune(e)
+					}
+				}
+			}
+		}
+	}
+}
