00e133be0747a355cbf99b65a4e1e109f1782434
brksygmr <brksygmr@outlook.com>
2021-09-08 16:37:48 +0100

    printcomb

--- printcomb.go
+++ printcomb.go
@@ -3,25 +3,23 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	var aRune string = "789"
-	for a := '0'; a <= '7'; a++ {
-		for b := a + 1; b <= '8'; b++ {
-			for c := b + 1; c <= '9'; c++ {
-				for d := ','; d <= ','; d++ {
-					for e := ' '; e <= ' '; e++ {
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9'; c++ {
+				if a < b && b < c && a < c && a != b && b != c && a != '7' {
 
-						z01.PrintRune(a)
-						z01.PrintRune(b)
-						z01.PrintRune(c)
-						z01.PrintRune(d)
-						z01.PrintRune(e)
-					}
+					z01.PrintRune(rune(a))
+					z01.PrintRune(rune(b))
+					z01.PrintRune(rune(c))
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
+				} else if a == '7' && b == '8' && c == '9' {
+					z01.PrintRune(rune(a))
+					z01.PrintRune(rune(b))
+					z01.PrintRune(rune(c))
+					z01.PrintRune('\n')
 				}
 			}
 		}
 	}
-	z01.PrintRune(rune(aRune[0]))
-	z01.PrintRune(rune(aRune[1]))
-	z01.PrintRune(rune(aRune[2]))
-	z01.PrintRune('\n')
 }
