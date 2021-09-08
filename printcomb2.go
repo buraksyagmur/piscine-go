4d8d36dfcda18e0337a0529b24394956e1f15866
brksygmr <brksygmr@outlook.com>
2021-09-08 18:05:46 +0100

    printcomb2

--- printcomb2.go
+++ printcomb2.go
@@ -3,30 +3,27 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for a := '0'; a <= '9'; a = a + 1 {
-		for b := '0'; b <= '9'; b = b + 1 {
-			for c := '0'; c <= '9'; c = c + 1 {
-				for d := '0'; d <= '9'; d = d + 1 {
 
-					z01.PrintRune(rune(a))
-					z01.PrintRune(rune(b))
-					z01.PrintRune(' ')
-					z01.PrintRune(rune(c))
-					z01.PrintRune(rune(d))
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
 
-					if a == '9' && b == '9' && c == '9' && d == '9' {
-						z01.PrintRune(rune(a))
-						z01.PrintRune(rune(b))
-						z01.PrintRune(' ')
-						z01.PrintRune(rune(c))
-						z01.PrintRune(rune(d))
+    for a := '0'; a <= '9'; a++ {
+        for b := '0'; b <= '9'; b++ {
+            
+            for c := a; c <= '9'; c++ {
+                for d := b + 1 ; d <= '9'; d++ {
+                    z01.PrintRune(a)
+                    z01.PrintRune(b)
+                    z01.PrintRune(' ')
+                    z01.PrintRune(c)
+                    z01.PrintRune(d)
+                    if a < '9' || b < '8' || c < '9' || d < '9' {
+                        z01.PrintRune(',')
+                        z01.PrintRune(' ')
+                    }
 
-					}
-				}
-			}
-		}
-	}
-	z01.PrintRune('\n')
+                }
+               
+            }
+        }
+   z01.PrintRune('\n') 
 }
+} 
