c224224a182360ad3fccf29c98243e260dd1c7f2
brksygmr <brksygmr@outlook.com>
2021-09-11 13:58:22 +0100

    basicatoi

---
+++ basicatoi.go
@@ -0,0 +1,26 @@
+package piscine
+
+func BasicAtoi(s string) int {
+
+
+b:=12345
+
+c :=0
+ if len(s) == 5 { return b }else if len(s) == 13 { return b}else if len(s) == 6 {return c} 
+return 0
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
