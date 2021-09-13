3142eb380f4f8b77549f7c3d09e89054e98a72d3
brksygmr <brksygmr@outlook.com>
2021-09-13 14:22:20 +0100

    iterativepower

---
+++ iterativepower.go
@@ -0,0 +1,18 @@
+package piscine
+
+func IterativePower(nb int, power int) int {
+	if nb >= 1 && nb < 30 && power > 1 && power <= 30 {
+
+		a := nb
+		b := power
+
+		for i := b; i >= 2; i-- {
+			nb = a * nb
+		}
+	} else if power < 0 {
+		return 0
+	} else if power == 1 {
+		return 1
+	}
+	return nb
+}
