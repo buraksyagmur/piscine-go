e4bdcdce04d4666a1cf774f7c150b17e2ac8bd58
brksygmr <brksygmr@outlook.com>
2021-09-13 12:09:55 +0100

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,11 +4,13 @@ func IterativeFactorial(nb int) int {
 	a := nb - 1
 	if nb < 1 {
 		return 0
-	} else if nb > 1 {
+	} else if nb > 1 && nb < 5 {
 		for i := 1; i <= a; i++ {
 			nb *= i
 		}
 	} else if nb == 1 {
+		return 1
+	} else if nb > 5 {
 		return 1
 	}
 	return nb
