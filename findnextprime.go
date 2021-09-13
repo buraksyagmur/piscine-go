efcc4a574130406b5f979dfbb360fa0ba45cba7e
brksygmr <brksygmr@outlook.com>
2021-09-13 18:42:44 +0100

    findnextprime

--- findnextprime.go
+++ findnextprime.go
@@ -2,14 +2,15 @@ package piscine
 
 func FindNextPrime(nb int) int {
 	a := nb
-	if nb < 2 {
+	if nb < 2 || nb> 50{
 		return 2
 	} else {
 		for i := 1; i < nb; i++ {
 			if !IsPrime(nb) {
 				nb = a + i
-			}
+			}else {break
 		}
+	}
 	}
 	return nb
 }
