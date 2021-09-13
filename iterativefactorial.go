31c27fc56a13b69467010a95027eb517ed83f535
brksygmr <brksygmr@outlook.com>
2021-09-13 12:06:07 +0100

    iterativefactorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,13 +1,15 @@
 package piscine
 
 func IterativeFactorial(nb int) int {
-	if nb <= 1 {
+	a := nb - 1
+	if nb < 1 {
 		return 0
 	} else if nb > 1 {
-		a := nb - 1
 		for i := 1; i <= a; i++ {
 			nb *= i
 		}
+	} else if nb == 1 {
+		return 1
 	}
 	return nb
 }
