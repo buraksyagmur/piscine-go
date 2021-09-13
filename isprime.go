2a005f699692caa9756bcb9592b3d850c9f8a2a7
brksygmr <brksygmr@outlook.com>
2021-09-13 16:53:17 +0100

    isprime

--- isprime.go
+++ isprime.go
@@ -1,11 +1,15 @@
 package piscine
 
 func IsPrime(nb int) bool {
-	a := nb
-	for i := 2; i < a; i++ {
-		if a%i == 0 {
-			return false
+	if nb < 2 {
+		return false
+	} else {
+		a := nb
+		for i := 2; i < a; i++ {
+			if a%i == 0 {
+				return false
+			}
 		}
+		return true
 	}
-	return true
 }
