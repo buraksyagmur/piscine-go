5b3ab6aa7954c93e74044282780ef556d62a8dfd
brksygmr <brksygmr@outlook.com>
2021-09-14 17:05:33 +0100

    toupper

---
+++ toupper.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ToUpper(s string) string {
+	a := []rune(s)
+	b := len(a) - 1
+	c := ""
+	for i := 0; i <= b; i++ {
+		if a[i] >= 97 && a[i] <= 127 {
+			a[i] = a[i] - 32
+		}
+		c += string(a[i])
+
+	}
+	return c
+}
