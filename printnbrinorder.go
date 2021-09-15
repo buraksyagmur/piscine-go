2be57d4333c34f02b168ec118b9481fd49d28797
brksygmr <brksygmr@outlook.com>
2021-09-15 15:11:06 +0100

    printnbrinorder

---
+++ printnbrinorder.go
@@ -0,0 +1,23 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func PrintNbrInOrder(n int) {
+	
+	if n == 0 {
+		z01.PrintRune('0')
+		return
+	}
+	var b [10]int
+	for n != 0 {
+
+		b[n%10]++
+		n /=10
+	}
+	for i := 0; i < 9; i++ {
+		for b[i] >0 {
+			z01.PrintRune(rune(i) + '0')
+			b[i]--
+		}
+	}
+}
