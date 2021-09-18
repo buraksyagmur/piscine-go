bd8d3e7f221eb4892d58c2842154fffd0a5cfd62
brksygmr <brksygmr@outlook.com>
2021-09-18 17:31:14 +0100

    splitspaces

---
+++ splitwhitespaces.go
@@ -0,0 +1,21 @@
+package piscine
+
+func SplitWhiteSpaces(s string) []string {
+	result := ""
+	var result2 []string
+	a := len(s)
+	srune := []rune(s)
+	for i := 0; i < a; i++ {
+		if srune[i] == 32 || srune[i] == '\t' || srune[i] == '\n' {
+
+			result2 = append(result2, result)
+			result = ""
+		} else {
+			result += string(srune[i])
+		}
+		if i == a-1 {
+			result2 = append(result2, result)
+		}
+	}
+	return result2
+}
