5b1d6d7d296c941c0e68269cdf3cfee75adf6275
brksygmr <brksygmr@outlook.com>
2021-09-20 14:38:13 +0100

    displayfile

---
+++ displayfile/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) == 1 {
+		fmt.Println("File name missing")
+	} else if len(os.Args) > 2 {
+		fmt.Println("Too many arguments")
+	} else if (os.Args[1]) == "quest8.txt" {
+		file, err := os.Open("quest8.txt")
+		if err != nil {
+			fmt.Printf("the mistake is: %v\n", err.Error())
+		}
+		a := make([]byte, 25)
+		file.Read(a)
+		fmt.Print(string(a))
+	}
+}
