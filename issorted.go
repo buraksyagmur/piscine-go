5c8f8f2a06c03024001f5c0734bd2e0036116f92
brksygmr <brksygmr@outlook.com>
2021-09-21 16:10:25 +0100

    issorted

---
+++ issorted.go
@@ -0,0 +1,37 @@
+package piscine
+
+func IsSorted(f func(a, b int) int, a []int) bool {
+	y := true
+	w := true
+
+	for i := 1; i < len(a); i++ {
+		if !(f(a[i-1], a[i]) >= 0) {
+			y = false
+		}
+	}
+
+	for i := 1; i < len(a); i++ {
+		if !(f(a[i-1], a[i]) <= 0) {
+			w = false
+		}
+	}
+	return w || y
+}
+
+func isSuperior(a, b int) int {
+	if a > b {
+		return 1
+	} else if a == b {
+		return 0
+	} else {
+		return -1
+	}
+}
+
+func lenghtof(d []int) int {
+	i := 0
+	for _, _ = range d {
+		i++
+	}
+	return i
+}
