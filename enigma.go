7f0748e2f39e18950520b197f83e51d36cd54fba
brksygmr <brksygmr@outlook.com>
2021-09-22 14:16:21 +0100

    enigma

---
+++ enigma.go
@@ -0,0 +1,11 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	realc := *******c
+	*******c = ***a
+	reald := ****d
+	****d = realc
+	realb := *b
+	***a = realb
+	*b = reald
+}
