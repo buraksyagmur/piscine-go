015e5bd3f9080570f5d83fa3e5c06eb82be8709e
brksygmr <brksygmr@outlook.com>
2021-09-22 13:57:31 +0100

    comcheck

--- comcheck/main.go
+++ comcheck/main.go
@@ -12,6 +12,7 @@ func main() {
 	for i := 0; i < lenA; i++ {
 		if a[i] == "01" || a[i] == "galaxy" || a[i] == "galaxy 01" {
 			fmt.Println("Alert!!!")
+			break
 		} else {
 			// fmt.Println(nil)
 		}
