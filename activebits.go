1c8333a09965fe4fe470d751a86a138ef2d6df71
brksygmr <brksygmr@outlook.com>
2021-09-22 17:55:21 +0100

    activebits

--- activebits.go
+++ activebits.go
@@ -1,5 +1,15 @@
 package piscine
 
-func ActiveBits(n int) int {
-	return (n - 4)
+func ActiveBits(n int) uint {
+	d := 1
+
+	for n != 1 {
+		if n == 1 {
+			break
+		} else {
+			n = n / 2
+			d += n % 2
+		}
+	}
+	return uint(d)
 }
