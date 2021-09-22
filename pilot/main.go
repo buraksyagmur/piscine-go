77148b0e758ecc6006d862e9b4db8a4b17415005
brksygmr <brksygmr@outlook.com>
2021-09-22 15:13:50 +0100

    pilot

---
+++ pilot/main.go
@@ -0,0 +1,24 @@
+package main
+
+import ("fmt"
+)
+
+type Pilot struct{
+	Name string
+	Life int
+	Age int
+	Aircraft string
+	
+}
+
+
+func main() {
+AIRCRAFT1:= "1"
+	var donnie Pilot
+	donnie.Name = "Donnie"
+	donnie.Life = 100.0
+	donnie.Age = 24
+	donnie.Aircraft = AIRCRAFT1
+
+	fmt.Println(donnie)
+}
