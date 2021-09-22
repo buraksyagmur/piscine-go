cc881260342941c938a673521c4f111ecd603ac3
brksygmr <brksygmr@outlook.com>
2021-09-22 13:36:05 +0100

    collatzcountdown

---
+++ collatzcountdown.go
@@ -0,0 +1,19 @@
+package piscine
+
+func CollatzCountdown(start int) int {
+	if start <= 0 {
+		return -1
+	}
+
+	result := 1
+	for start != 1 {
+
+		if start%2 == 0 {
+			start = start / 2
+		} else if start%2 != 0 {
+			start = (start * 3) + 1
+		}
+		result++
+	}
+	return result
+}
