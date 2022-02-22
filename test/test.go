4ad997aa18e104496d88a349b5a6f46678ad5499
brksygmr <brksygmr@outlook.com>
2022-02-22 13:09:48 +0000

    listpushback

--- test/test.go
+++ test/test.go
@@ -6,7 +6,56 @@ import (
 )
 
 func main() {
-	fmt.Println(piscine.BasicAtoi("12345"))
-	fmt.Println(piscine.BasicAtoi("0000000012345"))
-	fmt.Println(piscine.BasicAtoi("000000"))
+	link := &piscine.List{}
+
+	piscine.ListPushBack(link, "Hello")
+	piscine.ListPushBack(link, "man")
+	piscine.ListPushBack(link, "how are you")
+
+	for link.Head != nil {
+		fmt.Println(link.Head.Data)
+		link.Head = link.Head.Next
+	}
 }
+
+
+
+
+
+
+
+
+
+
+
+// type node struct {
+// 	data int
+// 	next *node
+// }
+
+// func insert(head *node, data int) *node {
+// 	n := &node{data: data}
+// 	if head == nil {
+// 		return n
+// 	} else {
+// 		n.next = head
+
+// 		return n
+// 	}
+// }
+
+// func Printlist(head *node) {
+// 	for head != nil {
+// 		fmt.Print(head.data, "->")
+// 		head = head.next
+
+// 	}
+// 	fmt.Println(nil)
+// }
+
+// func main() {
+// 	var link *node
+// 	link = insert(link, 1)
+// 	// link = insert(link, 2)
+// 	Printlist(link)
+// }
